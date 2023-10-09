2bc41e23b0dbf128b876216c180d9f01e4a2b0a8
Daniel Vargas Castro <vargasdaniel195@gmail.com>
2023-10-09 11:12:26 -0600

    Updating proyect
    Updating page1.html, new page2.html, updating script.js, delete funcionalidades.js

--- Segundo proyecto/backend/funcionalidades.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    $(".menu-btn").click(function(){
-        $(".navbar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
-    });
-});
--- Segundo proyecto/backend/script.js
+++ Segundo proyecto/backend/script.js
@@ -4,44 +4,55 @@ $(document).ready(function () {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var data = JSON.parse(xhr.responseText);
-            var cards = document.querySelectorAll(".card");
-            var loadedImagesCount = 0;
-
-            function loadAsyncImage(imageUrl, imgElement, callback) {
-                var img = new Image();
-                img.onload = function () {
-                    imgElement.src = imageUrl;
-                    callback();
-                };
-                img.src = imageUrl;
-            }
-
-            function loadAll() {
-                for (var i = 0; i < cards.length; i++) {
-                    var imagen = data[i].imagen;
-                    var imgElement = cards[i].querySelector(".img img");
-                    loadAsyncImage(imagen, imgElement, function () {
-                        loadedImagesCount++;
-                        if (loadedImagesCount === cards.length) {
-                        }
-                    });
-
-                    var descripcion = data[i].descripcion;
-                    var descElement = cards[i].querySelector(".desc");
-                    descElement.innerText = descripcion;
-
-                    var precio = data[i].precio
-                    var precioElement = cards[i].querySelector(".price");
-                    precioElement.innerText = "₡" + precio;
-
-                    var nombre = data[i].nombre
-                    var nombreElement = cards[i].querySelector(".title");
-                    nombreElement.innerText = nombre;
-                }
-            }
-
-            loadAll();
+            var filterCheckboxes = document.querySelectorAll('.filter-checkbox');
+            // Asignar un evento de escucha a los checkboxes de filtro
+            updateResults(data);
+            filterCheckboxes.forEach(function(checkbox) {
+                
+                checkbox.addEventListener('change', function() {
+                    
+                    updateResults(data);
+                    
+                });
+            });
         }
     };
     xhr.send();
 });
+
+function updateResults(data) {
+    
+    var resultsSection = document.getElementById('products');
+    resultsSection.innerHTML = '';  // Limpiamos la sección de resultados
+
+    var selectedCategories = Array.from(document.querySelectorAll('.filter-checkbox:checked'))
+                                .map(function(checkbox) {
+                                    return checkbox.value;
+                                });
+
+    if (selectedCategories.length === 0) {
+        // Si no se ha seleccionado ninguna categoría, mostrar todas las tarjetas
+        data.forEach(function(item) {
+            
+            renderCard(item, resultsSection);
+        });
+    } else {
+        // Mostrar solo las tarjetas de las categorías seleccionadas
+        data.forEach(function(item) {
+            if (selectedCategories.includes(item.categoria)) {
+                renderCard(item, resultsSection);
+            }
+        });
+    }
+}
+
+function renderCard(item, container) {
+    var card = document.createElement('div');
+    card.classList.add('card');
+    
+    // Construir el contenido de la tarjeta según tus necesidades
+    // Aquí solo se muestra el nombre
+    card.innerHTML = '<div class="title">' + item.nombre + '</div>';
+    
+    container.appendChild(card);
+}
